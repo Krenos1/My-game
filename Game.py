a87f8287faeedec4f43815353e7891c0114e1527
Krenos1 <51523765+Krenos1@users.noreply.github.com>
2024-01-27 19:57:29 -0500

    update 1.1

--- Game.py
+++ Game.py
@@ -4,17 +4,15 @@ from sys import exit
 #Fonction qui permet d'afficher le score du joueur 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = testFont.render(f'{current_time}',False, (64,64,64))
+    score_surf = testFont.render(f'{current_time}',False,(64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf,score_rect)
-
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-#Permet le début de ma loop qui permet le fonctionnement du jeu 
 game = True
 #GameAcitve permet de savoir si le jeu est en pause ou non 
 gameActive = True 
@@ -35,10 +33,10 @@ snailRect = snailSurface.get_rect(midbottom = (600,300))
 
 playerSurf = pygame.image.load('TestSprites/player_walk_1.png').convert_alpha()
 playerRect = playerSurf.get_rect(midbottom = (80,300))
-
 #control de la gravité
 playerGravity = 0
-
+playerStand = pygame.image.load('TestSprites/player_stand.png')
+playerStandRect = playerStand.get_rect(center = (400,200))
 
 while game == True:
     for event in pygame.event.get():
@@ -64,7 +62,7 @@ while game == True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 gameActive = True  
                 snailRect.left = 800
-                start_time = pygame.time.get_ticks()
+                start_time = int(pygame.time.get_ticks() // 1000)
     
     if gameActive == True:    
         screen.blit(skySurface,(0,0))
@@ -109,10 +107,11 @@ while game == True:
         # collision
         if snailRect.colliderect(playerRect):
             gameActive = False
-    
-    else:
-        screen.fill('Green')
 
+    #Game over screen
+    else:
+        screen.fill((95,95,160))
+        screen.blit(playerStand, playerStandRect)
     pygame.display.update()
     #Rafraichissement de l'ecran par seconde
     clock.tick(60)
